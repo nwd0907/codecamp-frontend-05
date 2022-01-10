d896102a32135f21864c987c51fcec9ca7c4b13e
nwd0907 <nwd0907@newbizstart.com>
2022-01-11 08:35:09 +0900

    first commit

---
+++ class/pages/01-01-mypage/index.js
@@ -0,0 +1,9 @@
+export default function Mypage(){
+
+    return (
+        <div>
+            <div>안녕하세요~~</div>
+            <input type="text"/>
+        </div>
+    )
+}
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,12 @@
+import {MyTitle, MyEmail} from '../../styles/emotion'
+
+export default function EmotionPage(){
+
+    return (
+        <div>
+            <MyTitle>안녕하세요 이모션 페이지입니다.~~</MyTitle>
+            <input type="text"/>
+            <img src="/images/lotto.png" />
+        </div>
+    )
+}
