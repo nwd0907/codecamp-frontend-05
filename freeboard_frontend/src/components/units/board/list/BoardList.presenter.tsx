270e1c80e855aeeef374ecc39bddf613e9a63e2c
nwd0907 <nwd0907@newbizstart.com>
2022-01-27 22:16:50 +0900

    Update pagination

--- freeboard_frontend/src/components/units/board/list/BoardList.presenter.tsx
+++ freeboard_frontend/src/components/units/board/list/BoardList.presenter.tsx
@@ -1,18 +1,22 @@
 import { getMyDate } from "../../../../commons/libraries/utils";
+import Paginations01 from "../../../commons/paginations/01/Paginations01.container";
 import * as S from "./BoardList.styles";
+import { IBoardListUIProps } from "./BoardList.types";
 
-export default function BoardListUI(props) {
+export default function BoardListUI(props: IBoardListUIProps) {
   return (
     <S.Wrapper>
       <S.Row>
-        <S.ColumnHeaderBasic>번호</S.ColumnHeaderBasic>
+        <S.ColumnHeaderBasic>ID</S.ColumnHeaderBasic>
         <S.ColumnHeaderTitle>제목</S.ColumnHeaderTitle>
         <S.ColumnHeaderBasic>작성자</S.ColumnHeaderBasic>
         <S.ColumnHeaderBasic>날짜</S.ColumnHeaderBasic>
       </S.Row>
       {props.data?.fetchBoards.map((el, index) => (
         <S.Row key={el._id}>
-          <S.ColumnBasic>{index + 1}</S.ColumnBasic>
+          <S.ColumnBasic>
+            {String(el._id).slice(-4).toUpperCase()}
+          </S.ColumnBasic>
           <S.ColumnTitle id={el._id} onClick={props.onClickMoveToBoardDetail}>
             {el.title}
           </S.ColumnTitle>
@@ -22,6 +26,7 @@ export default function BoardListUI(props) {
       ))}
       <S.TableBottom />
       <S.Footer>
+        <Paginations01 refetch={props.refetch} count={props.count} />
         <S.Button onClick={props.onClickMoveToBoardNew}>
           <S.PencilIcon src="/images/board/list/write.png" />
           게시물 등록하기
