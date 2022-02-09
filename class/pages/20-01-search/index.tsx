c1ca8771328d6ee5fc6e1c876d4c9f3e733fee30
nwd0907 <nwd0907@newbizstart.com>
2022-02-09 21:22:35 +0900

    Update search

---
+++ class/pages/20-01-search/index.tsx
@@ -0,0 +1,57 @@
+import { useQuery, gql } from "@apollo/client";
+import { ChangeEvent, MouseEvent, useState } from "react";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($search: String, $page: Int) {
+    fetchBoards(search: $search, page: $page) {
+      _id
+      writer
+      title
+    }
+  }
+`;
+
+export default function SearchPage() {
+  const [search, setSearch] = useState("");
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const onClickSearch = () => {
+    refetch({ search: search, page: 1 });
+  };
+
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>) => {
+    if (event.target instanceof Element)
+      refetch({ page: Number(event.target.id) });
+  };
+
+  return (
+    <div>
+      <h1>검색 페이지!!!</h1>
+      검색어입력: <input type="text" onChange={onChangeSearch} />
+      <button onClick={onClickSearch}>검색</button>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span> {el.writer} </span>
+          <span> {el.title} </span>
+        </div>
+      ))}
+      {new Array(10).fill(1).map((_, index) => (
+        <span key={index + 1} onClick={onClickPage} id={String(index + 1)}>
+          {` ${index + 1} `}
+        </span>
+      ))}
+    </div>
+  );
+}
