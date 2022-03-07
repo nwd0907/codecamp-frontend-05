a25c75482420b58f9dafcb7d32756d98013e054c
nwd0907 <nwd0907@newbizstart.com>
2022-03-07 11:21:18 +0900

    Update dynamic-routing

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function BoardsDetailPage() {
-  return <div>안녕하세요! 게시글 상세페이지 입니다!!!</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      안녕하세요! 게시글 상세페이지 입니다!!!, 게시글ID는 {router.query.boardId}
+      입니다!!!
+    </div>
+  );
 }
