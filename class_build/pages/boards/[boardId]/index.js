af9b168a9d518de26a5c653f962aee0f810bc667
nwd0907 <nwd0907@newbizstart.com>
2022-03-07 13:06:33 +0900

    Update ssr

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,21 +1,17 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { gql, request } from "graphql-request";
 
-export default function BoardsDetailPage() {
+export default function BoardsDetailPage(props) {
   const router = useRouter();
+  // const { data } = useQuery(FETCH_BOARD);
 
   return (
     <div>
       <Head>
-        <meta property="og:title" content="나의 게시판입니다" />
-        <meta
-          property="og:description"
-          content="저의 게시판에 오신것을 환영합니다!!!"
-        />
-        <meta
-          property="og:image"
-          content="https://dullyshin.github.io/2018/08/30/HTML-imgLink/#lg=1&slide=0"
-        />
+        <meta property="og:title" content={props.myboardData.title} />
+        <meta property="og:description" content={props.myboardData.contents} />
+        <meta property="og:image" content={props.myboardData.images[0]} />
       </Head>
       <div>
         안녕하세요! 게시글 상세페이지 입니다!!!, 게시글ID는
@@ -25,3 +21,33 @@ export default function BoardsDetailPage() {
     </div>
   );
 }
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
+
+// 이 페이지는 서버사이드 렌더링 할래!!
+export const getServerSideProps = async (context) => {
+  // 데이터를 요청할 것!!!
+  const result = await request(
+    "https://backend05.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    { boardId: context.query.boardId }
+  );
+
+  return {
+    props: {
+      myboardData: {
+        title: result.fetchBoard.title,
+        contents: result.fetchBoard.contents,
+        images: result.fetchBoard.images,
+      },
+    },
+  };
+};
