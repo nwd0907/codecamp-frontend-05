4dde16d98e17f4260f7449f4719a029f1edfb457
nwd0907 <nwd0907@newbizstart.com>
2022-03-07 12:28:31 +0900

    Update opengraph

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,12 +1,27 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function BoardsDetailPage() {
   const router = useRouter();
 
   return (
     <div>
-      안녕하세요! 게시글 상세페이지 입니다!!!, 게시글ID는 {router.query.boardId}
-      입니다!!!
+      <Head>
+        <meta property="og:title" content="나의 게시판입니다" />
+        <meta
+          property="og:description"
+          content="저의 게시판에 오신것을 환영합니다!!!"
+        />
+        <meta
+          property="og:image"
+          content="https://dullyshin.github.io/2018/08/30/HTML-imgLink/#lg=1&slide=0"
+        />
+      </Head>
+      <div>
+        안녕하세요! 게시글 상세페이지 입니다!!!, 게시글ID는
+        {router.query.boardId}
+        입니다!!!
+      </div>
     </div>
   );
 }
