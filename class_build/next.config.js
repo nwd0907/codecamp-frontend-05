9f41a96fa7ec28f5e0b85757f390dc33302b03aa
nwd0907 <nwd0907@newbizstart.com>
2022-03-08 12:39:01 +0900

    Update docker

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp_deploy_5",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
